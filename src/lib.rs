cad18ea94bfd13dfc94b2dcccab0111ba933014b
Roy Magen <magenroy@gmail.com>
2025-02-11 16:01:12 -0500

    Revert "non-function ParamRoute component"
    This reverts commit 552c6dd7848eaf1bead4fc21ff4bf206fd39b231.

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,9 @@ pub mod app;
 pub mod views;
 
 use futures::{channel::mpsc, Stream};
-use leptos::{children, prelude::*};
-use leptos_router::{ChooseView, MatchNestedRoutes, PossibleRouteMatch};
-use std::{
-    fs::DirEntry, future::Future, path::{Path, PathBuf}
-};
+use leptos::prelude::*;
+use leptos_router::MatchNestedRoutes;
+use std::{future::Future, path::{Path, PathBuf}};
 
 // const PREFIX: &str = "/website-test";
 pub fn prefix() -> String {
@@ -32,14 +30,11 @@ macro_rules! prefixed {
 }
 
 pub mod prelude {
+    pub use super::list_slugs;
     pub use super::prefix;
     pub use super::prefixed;
     pub use super::watch_path;
     pub use super::with_prefix;
-
-    pub use super::list_slugs;
-    pub use super::ParamRoute;
-
     pub use leptos::prelude::*;
 }
 
@@ -52,100 +47,18 @@ pub fn hydrate() {
 }
 
 // #[component(transparent)]
-// pub fn ParamRoute(param_name: &'static str, view: impl ChooseView) -> impl MatchNestedRoutes + Clone {
-//     use leptos_router::components::Route;
+// pub fn ParametrizedRoute(param_name: &'static str, view: impl IntoView, f: Fn() -> impl Future<Vec<String>>) -> impl MatchNestedRoutes + Clone {
 //     view! {
-//         <Route path=(leptos_router::ParamSegment(param_name),) view/>
-//     }.into_inner()
+//         <Route
+//             path=(leptos_router::ParamSegment(param_name)),
+//             view=view,
+//             ssr=SsrMode::Static(
+//                 StaticRoute::new()
+//                     .prerender_params(|| async move {})
+//             )
+//         />
+//     }
 // }
-
-#[server]
-pub async fn scan_dir(path: String) -> Result<Vec<PathBuf>, ServerFnError> {
-    use tokio::fs;
-    use tokio_stream::wrappers::ReadDirStream;
-    use tokio_stream::StreamExt;
-
-    println!("READING {path}");
-    let files = ReadDirStream::new(fs::read_dir(&path).await?);
-
-    // let files = std::fs::read_dir(&path).unwrap();
-
-    let out = files.filter_map(
-        |d| {
-            println!("{:?}", d);
-            Some(d.ok()?.path())
-            // let p = d.ok()?.path();
-            // Some(p.to_str()?.to_string())
-            // // if p.is_file() {
-            // //     Some(format!("asdf/{}",p.file_stem()?.to_str()?))
-            // // } else { None }
-        }
-    ).collect();
-
-    Ok(out.await)
-}
-
-#[component(transparent)]
-pub fn ParamRoute(
-    #[prop(default="slug")] param_name: &'static str,
-    view: impl ChooseView,
-    dir_path: &'static str,
-    // By default just reads all files in the directory, and outputs the stems
-    #[prop(optional)] mapper: Option<Box<dyn Fn(DirEntry) -> Option<String>>>,
-) -> impl MatchNestedRoutes + Clone {
-    use leptos_router::components::*;
-    use leptos_router::{path, ParamSegment, SsrMode, static_routes::{StaticRoute, StaticParamsMap}};
-
-    /* let default = |e: tokio::fs::DirEntry| -> Option<String> {
-        let p = e.path();
-        if p.is_file() {
-            Some(format!("asdf/{}",p.file_stem()?.to_str()?))
-        } else { None }
-    }; */
-    // let slugs = match &mapper {
-    //     _ => scan_dir(&dir_path, default),
-    //     // Some(f) => scan_dir(&dir_path, f)
-    // };
-    let slugs: Vec<String> = std::fs::read_dir(&dir_path).unwrap().
-        filter_map(|d| {
-            let out = match &mapper {
-                Some(f) => f(d.ok()?),
-                None => {
-                    let p = d.ok()?.path();
-                    if !p.is_file() { return None }
-                    Some(p.file_stem()?.to_str()?.to_string())
-                }
-            };
-            println!("{:?}", out);
-            out
-        }).collect();
-
-    println!("num of paths: {}", slugs.len());
-
-    view! {
-        <Route path=path!("/") view=move || view!{qwerty}/>
-        <Route
-            // path=(ParamSegment(param_name),)
-            path=path!("/:slug")
-            view=view
-            ssr=SsrMode::Static(
-                StaticRoute::new()
-                    .prerender_params(move || {
-        // need to take ownership of this variables before moving into async block
-        // and need to define slugs before the closure body
-        let slugs = slugs.clone();
-        async move {
-            // let slugs: Vec<String> = scan_dir(dir_path.to_string()).await.unwrap().into_iter().map(|p: PathBuf| -> String {p.to_str().unwrap().to_string()}).collect();
-            // let mut params = StaticParamsMap::new();
-            // params.insert(param_name, slugs);
-            // params
-            [("slug".into(), slugs)].into_iter().collect()
-    }})
-            )
-        />
-    }
-    .into_inner();
-}
 
 #[server]
 pub async fn list_slugs(path: PathBuf, extension: String) -> Result<Vec<String>, ServerFnError> {
@@ -180,7 +93,7 @@ pub async fn list_slugs(path: PathBuf, extension: String) -> Result<Vec<String>,
 // pub fn read_file<F,O,E>(path: impl AsRef<Path>, f: F) -> Result<Option<O>, E> where F: FnOnce(&str) -> Result<Option<O>, E> { }
 
 #[allow(unused)] // path is not used in non-SSR
-pub fn watch_path(path: impl AsRef<Path>) -> impl Stream<Item = ()> {
+pub fn watch_path(path: &Path) -> impl Stream<Item = ()> {
     #[allow(unused)]
     let (mut tx, rx) = mpsc::channel(0);
 
@@ -201,13 +114,9 @@ pub fn watch_path(path: impl AsRef<Path>) -> impl Stream<Item = ()> {
 
         // Add a path to be watched. All files and directories at that path and
         // below will be monitored for changes.
-        if watcher
-            .watch(path.as_ref(), RecursiveMode::NonRecursive)
-            .is_err()
-        {
-            leptos::logging::log!("could not watch path")
-        }
-        // .expect("could not watch path");
+        watcher
+            .watch(path, RecursiveMode::NonRecursive)
+            .expect("could not watch path");
 
         // we want this to run as long as the server is alive
         std::mem::forget(watcher);
